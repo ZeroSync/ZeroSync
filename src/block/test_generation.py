617e4084a32e56961be017881f5d125a74389901
SantiagoPittella <pittellasantiago@gmail.com>
2022-10-28 16:51:06 -0300

    Refactor tests generator

--- src/block/test_generation.py
+++ src/block/test_generation.py
@@ -12,22 +12,23 @@ f = open("test_bits_to_target.cairo", "w")
 # a dictionary
 io_json = json.load(io)
 
-f.write("// This is a auto generated test\n")
-f.write("%lang starknet\n")
-f.write("from block_header import bits_to_target, target_to_bits\n")
-f.write("from starkware.cairo.common.cairo_builtins import BitwiseBuiltin\n")
-
+f.write("// This is a auto generated test\n%lang starknet\nfrom block_header import bits_to_target, target_to_bits\nfrom starkware.cairo.common.cairo_builtins import BitwiseBuiltin\n\n")
 TAB = "    "
-tests_count = 1
-for io_case in io_json["bits_to_target"]:
-    f.write(f"@external\nfunc test_bits_to_target_small_works{tests_count}" + "{range_check_ptr, bitwise_ptr: BitwiseBuiltin*}() {\n")
-    f.write(TAB +"let bits = " + io_case["input"] + ";\n")
-    f.write(TAB +"let expected_target = " + io_case["expected_target"] + ";\n")
-    f.write(TAB + "let (result) = bits_to_target(bits);\n")
-    f.write(TAB + "assert result = expected_target;\n" )    
-    f.write(TAB + "return ();\n")
-    f.write("}\n\n")
-    tests_count += 1
+
+def generate_function(function_name, input, expected_output, counter):
+        return f"@external\nfunc test_{function_name}_{counter}" + "{range_check_ptr, bitwise_ptr: BitwiseBuiltin*}() {\n" + \
+        TAB +"let input = " + input + ";\n" + \
+        TAB +"let expected_target = " + expected_output + ";\n" + \
+        TAB + f"let (result) = {function_name}(input);\n" + \
+        TAB + "assert result = expected_target;\n"  + \
+        TAB + "return ();\n" + \
+        "}\n\n"
+
+for func_case in io_json.keys():
+    tests_count = 0
+    for io_case in io_json[func_case]:
+        f.write(generate_function(func_case, io_case["input"], io_case["expected_output"], tests_count))
+        tests_count += 1
 
 # Closing file
 io.close()
