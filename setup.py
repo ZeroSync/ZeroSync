db08a2e0f37f9ec4ec61e4f5a1ae1db983907058
LucasLvy <lucaslevy10@gmail.com>
2022-12-02 12:25:54 +0100

    chore: Python package structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="FILL",
+    version="FILL",
+    license="FILL",
+    author="FILL",
+    author_email="FILL",
+    packages=["ZeroSync"],
+    package_dir={"ZeroSync": "src/ZeroSync"},
+    include_package_data=True,
+    url="FILL",
+    keywords="FILL",
+)
