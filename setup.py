2fa0986a980d30b0b1ca1da8cef3b6e5c1447a69
Lukas <lucidLuckylee@users.noreply.github.com>
2022-12-02 17:27:02 +0100

    feat(pip): pip package setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# TODO fill missing information
+
+setup(
+    name="zerosync",
+    license="FILL",
+    author="FILL",
+    author_email="FILL",
+    packages=["ZeroSync"],
+    package_dir={"ZeroSync": ""},
+    include_package_data=True,
+    url="FILL",
+    keywords="FILL",
+)
