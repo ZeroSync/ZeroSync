9577ddf0e7bc77df9a6d961b64f20072e630dbf5
Lukas <lucidLuckylee@users.noreply.github.com>
2023-03-09 19:41:50 +0100

    fix(`chain_proof/main.py`): set NUM_OUTPUTS to 51

--- src/chain_proof/main.py
+++ src/chain_proof/main.py
@@ -11,8 +11,9 @@ parser.add_argument('--output_dir', type=str, default='tmp')
 args = parser.parse_args()
 
 P = 2**251 + 17 * 2**192 + 1
-NUM_OUTPUTS = 52
+NUM_OUTPUTS = 51
 FOLDING_FACTOR = 8
+
 
 class FeltsReader:
     def __init__(self, program_output):
@@ -21,11 +22,12 @@ class FeltsReader:
 
     def read(self):
         self.cursor += 1
-        return self.program_output[self.cursor-1]
+        return self.program_output[self.cursor - 1]
 
     def read_n(self, felt_count):
         self.cursor += felt_count
-        return self.program_output[ self.cursor-felt_count : self.cursor ]
+        return self.program_output[self.cursor - felt_count: self.cursor]
+
 
 def felts_to_hash(felts):
     res = 0
@@ -34,38 +36,41 @@ def felts_to_hash(felts):
         # Swap endianess
         felt = struct.unpack("<I", struct.pack(">I", felt))[0]
         res += pow(2**32, i) * felt
-    return hex(res).replace('0x','').zfill(64)
+    return hex(res).replace('0x', '').zfill(64)
+
 
 def felts_to_hex(felts):
-    return list( map(felt_to_hex, felts))
+    return list(map(felt_to_hex, felts))
+
 
 def felt_to_hex(felt):
     """
     Convert felts to hex representation.
     Remove leading "0x", pad leading zeros to 32 bytes
     """
-    hex_felt = hex(felt).replace('0x','').zfill(64)
-    if( int(hex_felt,16) ==0 ):
+    hex_felt = hex(felt).replace('0x', '').zfill(64)
+    if(int(hex_felt, 16) == 0):
         return "0"
     return hex_felt
 
+
 def parse_cairo_output(cairo_output, debug=False):
-    # Split at line break. Then cut off all lines until the start of the program output
+    # Split at line break. Then cut off all lines until the start of the
+    # program output
     lines = cairo_output.split('\n')
     start_index = lines.index('Program output:') + 1
 
     if debug:
         print('\n')
-        prints = lines[:start_index-1]
+        prints = lines[:start_index - 1]
         for line in prints:
             print(line)
-		
+
     lines = lines[start_index:]
     lines = [x for x in lines if x.strip() != '']
     lines = map(int, lines)
     lines = map(lambda x: x if x >= 0 else (x + P) % P, lines)
     return list(lines)
-
 
 
 output_dir = args.output_dir
@@ -92,16 +97,17 @@ genesis_state['program_length'] = len(program['data'])
 # TODO: compute program hash and write it into chain_state.json
 
 with open(f'{output_dir}/chain_state.json', 'w') as outfile:
-    outfile.write( json.dumps(genesis_state) )
+    outfile.write(json.dumps(genesis_state))
 
 chain_state_file = f'{output_dir}/chain_state.json'
 
-# The first Bitcoin TX ever occured in block 170. The second TX occured in block 181.
+# The first Bitcoin TX ever occured in block 170. The second TX occured in
+# block 181.
 start_block_height = 0
 end_block_height = 100
 for i in range(start_block_height, end_block_height):
     print(f'\n === Processing block height {i} ===')
-    
+
     # Run the Cairo runner
     print(f'Step 1: Cairo runner...')
     start_time = time.time()
@@ -119,22 +125,21 @@ for i in range(start_block_height, end_block_height):
     # Parse outputs
     r = FeltsReader(program_output)
     chain_state = {
-            'block_height' :     r.read(),
-            'best_block_hash' :  felts_to_hash( r.read_n(8) ),
-            'total_work' :       r.read(),
-            'current_target' :   r.read(),
-            'prev_timestamps' :  r.read_n(11),
-            'epoch_start_time' : r.read(),
-            'utreexo_roots' :    felts_to_hex( r.read_n(27) ),
-            'program_hash' :  	 hex( r.read() )
+        'block_height': r.read(),
+        'best_block_hash': felts_to_hash(r.read_n(8)),
+        'total_work': r.read(),
+        'current_target': r.read(),
+        'prev_timestamps': r.read_n(11),
+        'epoch_start_time': r.read(),
+        'utreexo_roots': felts_to_hex(r.read_n(27)),
+        'program_hash': hex(r.read())
     }
     # Write the chain state into a json file
     f = open(f'{output_dir}/chain_state.json', 'w')
     f.write(json.dumps(chain_state, indent=2))
     f.close()
 
-
-    # Run Giza prover 
+    # Run Giza prover
     print(f"Step 2: Giza prover...")
     start_time = time.time()
     cmd = f'giza prove                          \
@@ -150,4 +155,3 @@ for i in range(start_block_height, end_block_height):
         print(f'Done proving chain_proof-{i}\n')
     else:
         print(f'Proving failed. Error code: {return_code}\n')
-
