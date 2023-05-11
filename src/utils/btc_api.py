9f7928384c9be169d2bdad334b9630f732c8828c
Lukas <lucidLuckylee@users.noreply.github.com>
2023-05-11 23:29:26 +0200

    fix/btc_api): harcode coinbase tx

--- src/utils/btc_api.py
+++ src/utils/btc_api.py
@@ -12,6 +12,9 @@ CACHE_SIZE_SMALL_DATA = 10**5
 # BIG_DATA: blocks, txs
 CACHE_SIZE_LARGE_DATA = 10**3
 
+GENESIS_COINBASE_TX = r"""{"txid":"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b","version":1,"locktime":0,"vin":[{"txid":"0000000000000000000000000000000000000000000000000000000000000000","vout":4294967295,"prevout":null,"scriptsig":"04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73","scriptsig_asm":"OP_PUSHBYTES_4 ffff001d OP_PUSHBYTES_1 04 OP_PUSHBYTES_69 5468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73","is_coinbase":true,"sequence":4294967295}],"vout":[{"scriptpubkey":"4104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac","scriptpubkey_asm":"OP_PUSHBYTES_65 04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f OP_CHECKSIG","scriptpubkey_type":"p2pk","value":5000000000}],"size":204,"weight":816,"fee":0,"status":{"confirmed":true,"block_height":0,"block_hash":"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f","block_time":1231006505}}"""
+
+GENESIS_COINBASE_TX_RAW = r"""01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000"""
 
 def little_endian(string):
     splited = [str(string)[i: i + 2] for i in range(0, len(str(string)), 2)]
@@ -96,6 +99,7 @@ class BitcoinCLI(BTCAPI):
     def get_block(self, block_height):
         block_hash = self.get_block_hash(block_height)
         block = self.rpc.getblock(block_hash)
+        block['tx_count'] = block['nTx']
         return block
 
     def get_block_raw(self, block_height):
@@ -103,10 +107,11 @@ class BitcoinCLI(BTCAPI):
         block = self.rpc.getblock(block_hash, False)
         return block
 
-    # TODO hard code the genesis block
     # Expecting bitcoind client with -txindex
     @lru_cache(maxsize=CACHE_SIZE_LARGE_DATA)
     def get_transaction_raw(self, block_height, tx_index):
+        if block_height == 0 and tx_index == 0:
+            return GENESIS_COINBASE_TX_RAW
         block_hash = self.get_block_hash(block_height)
         tx_id = self.rpc.getblock(block_hash)['tx'][tx_index]
         tx_hex = self.rpc.getrawtransaction(f'{tx_id}')
@@ -114,12 +119,16 @@ class BitcoinCLI(BTCAPI):
 
     # Expecting bitcoind client with -txindex
     def get_transaction(self, block_height, tx_index):
+        if block_height == 0 and tx_index == 0:
+            return json.loads(GENESIS_COINBASE_TX)
         block_hash = self.get_block_hash(block_height)
         tx_id = self.rpc.getblock(block_hash)['tx'][tx_index]
         tx_json = self.rpc.getrawtransaction(f'{tx_id}', True)
         return tx_json
 
     def get_transaction_by_id(self, txid):
+        if tx_id == "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b":
+            return json.loads(GENESIS_COINBASE_TX)
         tx_json = self.rpc.getrawtransaction(f'{tx_id}', True)
         return tx_json
 
